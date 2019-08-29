906f15a0714a90ba2148e2e9d8771314234d5b48
brotwasme <brotwasme@gmail.com>
2019-08-29 14:56:50 +0100

    seting up binder

---
+++ refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.9'
+version = '0.1.9'
+full_version = '0.1.9.dev0+Unknown'
+git_revision = 'Unknown'
+release = False
+if not release:
+    version = full_version
